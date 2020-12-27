b59f115567a78faec84121a61e91518fad0d9fb3
kevinchen2046 <kevin-chen@foxmail.com>
2020-12-28 00:51:19 +0800

    x

--- src/BitmapDataFile.js
+++ src/BitmapDataFile.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { loadImage } = require('canvas');
+const Util = require('./Utils');
 
 module.exports = class BitmapDataFile {
 
@@ -12,25 +12,19 @@ module.exports = class BitmapDataFile {
         return new BitmapDataFile(url);
     }
     static save(path, bitmapData) {
-        fs.writeFileSync(path, bitmapData.canvas.toBuffer());
+        Util.save(path, bitmapData.canvas);
     }
 
     async load(url) {
         if (!url) return Promise.resolve(this);
-        var image = await loadImage(url);
-        this.bitmapData = new BitmapData(image.width, image.height, true, 0x00);
-        this.bitmapData.draw(image, new Matrix(1, 0, 0, 1, 0, 0), null);
-        return this;
+        return Util.load(url);
     }
 
     save(path) {
-        fs.writeFileSync(path, this.bitmapData.canvas.toBuffer());
+        Util.save(path, this.bitmapData.canvas);
     }
 
     saveToHTML(path) {
-        var coontent = `<head></head>\r\n<body>\r\n`;
-        coontent += `<img src="${this.bitmapData.canvas.toDataURL()}" />\r\n`;
-        coontent += `</body>`
-        fs.writeFileSync(path, coontent, 'utf-8');
+        Util.saveToHTML(path, this.bitmapData.canvas);
     }
 }
--- src/Utils.js
+++ src/Utils.js
@@ -1,14 +1,23 @@
-var fs = require('fs');
+const fs = require('fs');
+const { loadImage } = require('canvas');
 
-module.exports.saveToHTML = function (outhtml, ...canvases) {
+module.exports.load = async function (url) {
+    if (!url) return Promise.resolve(this);
+    var image = await loadImage(url);
+    var bitmapData = new BitmapData(image.width, image.height, true, 0x00);
+    bitmapData.draw(image, new Matrix(1, 0, 0, 1, 0, 0), null);
+    return bitmapData;
+}
+
+module.exports.saveToHTML = function (path, ...canvases) {
     var coontent = `<head></head>\r\n<body>\r\n`;
     for (var canvas of canvases) {
         coontent += `<img src="${canvas.toDataURL()}" />\r\n`;
     }
     coontent += `</body>`
-    fs.writeFileSync(outhtml, coontent, 'utf-8');
+    fs.writeFileSync(path, coontent, 'utf-8');
 }
 
-module.exports.save = function (outfile,canvas) {
-    fs.writeFileSync(outfile, canvas.toBuffer());
+module.exports.save = function (path, canvas) {
+    fs.writeFileSync(path, canvas.toBuffer());
 }
